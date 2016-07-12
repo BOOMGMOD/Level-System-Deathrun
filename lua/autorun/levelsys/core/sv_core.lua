9972cc8ddc7d387ff3e8b0183f8a2e3068df3a34
BOOMGMOD <caya1.carter1@gmail.com>
2016-07-11 23:13:07 -0500

    Added Group Fix

--- lua/autorun/levelsys/core/sv_core.lua
+++ lua/autorun/levelsys/core/sv_core.lua
@@ -54,6 +54,7 @@ end
 
 
 concommand.Add("levelsys_addxp", function(ply, cmd, args)
+	if ply:IsUserGroup("superadmin") then
 	for _, p in pairs(player.GetAll()) do
 		if p:SteamID() == args[1] and args[2] then
 			MsgC(Color(205,255,0), "XP given to " .. p:Nick() .. " in amount of " .. args[2] .. "\n")
@@ -62,9 +63,11 @@ concommand.Add("levelsys_addxp", function(ply, cmd, args)
 			MsgC(Color(255,95,115), "XP not given, no player found or no amount of XP specified.")
 		end
 	end
+	end
 end)
 
 concommand.Add("levelsys_removexp", function(ply, cmd, args)
+	if ply:IsUserGroup("superadmin") then
 	for _, p in pairs(player.GetAll()) do
 		if p:SteamID() == args[1] and args[2] then
 			MsgC(Color(205,255,0), "XP removed to " .. p:Nick() .. " in amount of " .. args[2] .. "\n")
@@ -72,6 +75,7 @@ concommand.Add("levelsys_removexp", function(ply, cmd, args)
 		else
 			MsgC(Color(255,95,115), "XP not removed, no player found or no amount of XP specified.")
 		end
+	end
 	end
 end)
 
