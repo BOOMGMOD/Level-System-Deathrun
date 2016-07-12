946690ece603c2c4048fb67d8cbc704248d33814
BOOMGMOD <caya1.carter1@gmail.com>
2016-07-11 23:20:24 -0500

    Moved Commands to Shared Core

--- lua/autorun/levelsys/core/sv_core.lua
+++ lua/autorun/levelsys/core/sv_core.lua
@@ -52,34 +52,6 @@ function levelsys.RemoveXP(ply, amount)
 	BroadcastData(ply)
 end
 
-
-concommand.Add("levelsys_addxp", function(ply, cmd, args)
-	if ply:IsUserGroup("superadmin") then
-	for _, p in pairs(player.GetAll()) do
-		if p:SteamID() == args[1] and args[2] then
-			MsgC(Color(205,255,0), "XP given to " .. p:Nick() .. " in amount of " .. args[2] .. "\n")
-			levelsys.AddXP(p, args[2])
-		else
-			MsgC(Color(255,95,115), "XP not given, no player found or no amount of XP specified.")
-		end
-	end
-	end
-end)
-
-concommand.Add("levelsys_removexp", function(ply, cmd, args)
-	if ply:IsUserGroup("superadmin") then
-	for _, p in pairs(player.GetAll()) do
-		if p:SteamID() == args[1] and args[2] then
-			MsgC(Color(205,255,0), "XP removed to " .. p:Nick() .. " in amount of " .. args[2] .. "\n")
-			levelsys.RemoveXP(p, args[2])
-		else
-			MsgC(Color(255,95,115), "XP not removed, no player found or no amount of XP specified.")
-		end
-	end
-	end
-end)
-
-
 /*function levelsys.FindLvlc(ply)
 	ply.getlvl = {}
 	ply.getlvl = levelsys.configdatabase.findlvl(ply)
